5d6e0fab5f55ba9289d2043657d9258e34ba6e70
Sameer Khan <sameer565413@gmail.com>
2022-08-09 10:10:54 +0530

    banner changes

--- src/app/fitness/home/home.component.ts
+++ src/app/fitness/home/home.component.ts
@@ -1,28 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import * as AOS from 'aos';
-import { ArticleService } from 'src/app/services/article.service';
-import { Article } from 'src/app/ViewModels/article';
+import { Component, OnInit } from "@angular/core";
+import * as AOS from "aos";
+import { ArticleService } from "src/app/services/article.service";
+import { Article } from "src/app/ViewModels/article";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-
   articles: Array<Article> = [];
   constructor(private articleService: ArticleService) {
     AOS.init();
-
   }
 
   ngOnInit(): void {
-    this.articleService
-          .getVisibleArticles(0)
-          .subscribe((data: any[]) => {
-            this.articles = data;
-          });
+    this.articleService.getVisibleArticles(0).subscribe((data: any[]) => {
+      this.articles = data;
+    });
   }
-
-
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({behavior: 'smooth'});
+  }
 }
